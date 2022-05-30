51ea472ecca1a0b41de126488d165e10dbfcd300
adriaandb <adriaan.debelder@gmail.com>
2022-05-30 19:57:42 +0200

    commit 220530b

--- classes/setup.py
+++ classes/setup.py
@@ -1,31 +1,7 @@
 from classes.db import Db
 
 
-class ActiveProject:
-    def __init__(self, projectID, name, artistID):
-        self.projectID=projectID
-        self.name=name
-        self.artistID=artistID
 
-    def load_project(self, projectID):
-        db=Db()
-        query = "SELECT * FROM projects; WHERE projectID=%s"
-        data = projectID
-        result = db.db_select(query,data)
-
-        if result and len(result) == 1:
-            for project in result:
-                self.projectID = project[0]
-                self.albumName = project[1]
-                self.artistID = project[3]
-                self.startDate = project[2]
-                self.endDate = project[4]
-
-
-
-class ActiveSession:
-    def __init__(self):
-        pass
 
 
 
